704589b8ec620b867e11daa14d4059da99aab581
OleksandrYaromenko <oleksandryaromenko21@gmail.com>
2024-07-28 10:16:08 +0200

    Update articles.api.js

--- src/components/API/articles.api.js
+++ src/components/API/articles.api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-axios.defaults.baseURL = "https://api.unspash.com";
+axios.defaults.baseURL = "https://api.unsplash.com";
 export const FechArticles = async (topic, page) => {
   const results = await axios.get(`/search/photos`, {
     params: {
